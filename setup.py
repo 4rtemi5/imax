0bd6ba139556edc7135f8e8b04341a316210b63e
4rtemi5 <raphael.pisoni@gmail.com>
2021-02-17 03:34:20 +0100

    prepare for pypi

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     url="https://github.com/4rtemi5/imax",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache 2.0 License",
+        "License :: OSI Approved :: Apache Software License v2.0",
         "Operating System :: OS Independent",
     ],
     packages=setuptools.find_packages(),
