a1a1e7e2a051dfaeecb833875f83ecfc4d25dd14
4rtemi5 <raphael.pisoni@gmail.com>
2021-02-17 20:01:30 +0100

    Update documentation

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="imax",
-    version="0.0.1-alpha3",
+    version="0.0.1-beta0",
     author="Raphael Pisoni",
     author_email="raphael.pisoni@gmail.com",
     description="Image augmentation library for Jax",
