fix weighting of pixel values

imax/project.py
@@ -139,7 +139,7 @@ def bilinear_project(points, depth, values, height, width, mask_value=0):
     w = w.at[ix, iy + 1].add(w2, mode="drop")
     w = w.at[ix + 1, iy].add(w3, mode="drop")
     w = w.at[ix + 1, iy + 1].add(w4, mode="drop")
-    #w += 1e-6
+    w = w[..., None]
 
     values = jnp.concatenate([
         values,
@@ -160,12 +160,17 @@ def bilinear_project(points, depth, values, height, width, mask_value=0):
     i = i.at[ix, iy + 1, iz].add(w2 * values, mode="drop")
     i = i.at[ix + 1, iy, iz].add(w3 * values, mode="drop")
     i = i.at[ix + 1, iy + 1, iz].add(w4 * values, mode="drop")
+    
+    i = jnp.where(
+        jnp.greater(w, 0),
+        i / w,
+        i
+    )
 
-    i = i / jnp.clip(w[..., None], 1e-6, 1.0) * jnp.greater(w[..., None], 0.0)
     values_out, depth_out = i[..., :-1], i[..., -1:]
 
     values_out = jnp.where(
-        jnp.equal(w[..., None], 0),
+        jnp.equal(w, 0),
         mask_value,
         values_out
     )

tests/test_transforms.py
@@ -119,7 +119,7 @@ def test_rotate90():
 
 
 def test_scale():
-    factor = 3
+    factor = 1
     inputs = jnp.pad(
         jnp.ones((1, 1, 4), dtype="uint8") * 255,
         ((1, 1), (1, 1), (0, 0)),

tests/utils.py
@@ -8,8 +8,6 @@ def compare(inputs, targets, outputs):
         for k, v in zip(['inputs', 'outputs', 'target'],
                         [inputs, outputs, targets]):
             print(k)
-            v = jnp.transpose(v, (2, 0, 1))
-            for channel in v:
-                print(channel)
+            print(v)
             print()
         raise ex